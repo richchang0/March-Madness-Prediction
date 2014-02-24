f117420f13e39d69a8fce4f5d7f0c75721d09340
Rich Chang <changaboy24@gmail.com>
2014-02-23 18:23:37 -0600

    get game ids

--- sportsdata.py
+++ sportsdata.py
@@ -9,15 +9,34 @@ def schedule(year, season):
 	url =  "http://api.sportsdatallc.org/ncaamb-"+access_level+version+"/games/"+year+"/"+season+"/schedule.xml?api_key="+apikey
 	clean = "{http://feed.elasticstats.com/schema/basketball/schedule-v2.0.xsd}"
 	response = urllib2.urlopen(url)
-	xmlprint(response, clean)
+	games(response, clean)
 
-def xmlprint(response, clean=""):
+def games(response, clean=""):
+	games = {}
+	hometeam = ""
+	awayteam = ""
 	tree = xml.parse(response)
 	root = tree.getroot()
 	for elem in root.iter():
-		print elem.tag.replace(clean,""), elem.attrib
+		tag = elem.tag.replace(clean,"")
+
+		if tag == "home":
+			hometeam = elem.attrib["name"]
+		elif tag == "away":
+			awayteam = elem.attrib["name"]
+		elif tag == "game":
+			games[hometeam+" vs "+awayteam] =  elem.attrib["id"]
+	pretty_print_games(games)
+	return games
+
+def pretty_print_games(games):
+	for game in games.keys():
+		print game
+		print games[game] + "\n"
+
 
 def main():
 	schedule("2013", "reg")
+
 
 main()
