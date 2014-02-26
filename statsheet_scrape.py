e31b2cf233f3c8e1ad5e9387c7b07184616fa08d
Allen <allenz456@gmail.com>
2014-02-25 21:16:28 -0600

    got all teams and statsheet urls

--- statsheet_scrape.py
+++ statsheet_scrape.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
+import string
 
 def get_page(url):
 
@@ -14,6 +15,25 @@ def get_page(url):
 	return soup
 
 def get_teams():
+
+	alphabet = list(string.ascii_uppercase)
+	baseUrl = "http://statsheet.com/mcb/teams/browse/name?t="
+
+	for letter in alphabet:
+		url = baseUrl + letter
+		soup = get_page(url)
+
+		for table in soup.findAll("tbody"):
+			for tr in table.findAll("tr"):
+				tds = tr.findAll("td")
+				if len(tds):
+					outputFile = open('teams_statsheet.txt', 'a')
+					line = str(tds[1].text) + "," + str(tds[1].a['href']) + "," + str(tds[2].text)
+					outputFile.write(line + "\n")
+					outputFile.close()
+					
+
+def get_stats():
 	url = 'http://statsheet.com/mcb/teams/syracuse/team_stats?type=all'
 	soup = get_page(url)
 	# print soup
