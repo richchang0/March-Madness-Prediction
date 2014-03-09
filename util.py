72068a81e40f16faf597e5aa22a151c72e3aef7a
Allen <allenz456@gmail.com>
2014-03-08 20:22:34 -0600

    mapped team names with their home court, some are missing bc statsheet didnt have stats for them

--- util.py
+++ util.py
@@ -85,10 +85,85 @@ def replace_statsheet_names():
 
 		modified_stat_file.write(line + "\n")
 	
+def keywithmaxval(d):
+     """ a) create a list of the dict's keys and values; 
+         b) return the key with the max value"""  
+     v=list(d.values())
+     k=list(d.keys())
+     return k[v.index(max(v))]
+
+
+def determine_home_court():
+	gameFile = open('textfiles/games_2012-2013.txt', 'r')
+
+	prevTeamName = ""
+
+	locations = {}
+
+	for line in gameFile:
+		splitLine = line.strip("\n").split(",")
+
+		curTeamName = splitLine[0]
+		curLocation = splitLine[3]
+
+		# print curTeamName
+		if curTeamName != prevTeamName:
+			# Skips the first instance
+			if prevTeamName != "":
+				homeCourt = keywithmaxval(locations)
+				print prevTeamName + "," + homeCourt
+				teamHomeFile = open('textfiles/team_home_matches.txt', 'a')
+				teamHomeFile.write(prevTeamName + "," + homeCourt + "\n")
+				teamHomeFile.close()
+			prevTeamName = curTeamName
+			locations = {}
+
+		# Build dictionary for getting the counts
+		if curLocation in locations:
+			locations[curLocation] += 1
+		else:
+			locations[curLocation] = 1
+
+def write_team_with_home():
+
+	homeFile = open('textfiles/team_home_matches.txt', 'r')
+	teamFile = open('textfiles/teams_url_names_statsheet.txt', 'r')
+
+	teamHomeMappings = {}
+
+	# build the team --> homecourt dictionary
+	for line in homeFile:
+		splitLine = line.strip("\n").split(",")
+		teamName = splitLine[0]
+		homeCourt = splitLine[1]
+
+		if teamName not in teamHomeMappings:
+			teamHomeMappings[teamName] = homeCourt
+
+	for team in teamFile:
+		splitLine = team.strip("\n").split(",")
+		teamName = splitLine[0]
+
+		# Default value for home if not found in dictionary
+		home = "NULL"
+
+		if teamName in teamHomeMappings:
+			home = teamHomeMappings[teamName]
+
+		splitLine.append(home)
+
+		newLine = ",".join(splitLine)
+
+		newTeamFile = open('textfiles/team_with_home_statsheet.txt', 'a')
+		newTeamFile.write(newLine + "\n")
+		newTeamFile.close()
+
 
 def main():
 	# get_missing_team_names()
 	# match_names()
 	# replace_statsheet_names()
+	# determine_home_court()
+	# write_team_with_home()
 
 main()
