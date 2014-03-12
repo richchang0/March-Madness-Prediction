7658fbf86a23022bd66b4f1fa3800067c745d547
Allen <allenz456@gmail.com>
2014-03-11 19:26:44 -0500

    latest commit

--- build_examples.py
+++ build_examples.py
@@ -2,7 +2,8 @@
 
 # Get the stats for the home and away team
 def get_stats(home,away):
-	stats = open("modified_stats.txt", "r")
+	fileName = 'stats_2012-2013'
+	stats = open("textfiles/" + fileName + ".txt", "r")
 
 	home_stats = []
 	away_stats = []
@@ -43,41 +44,44 @@ def calc_delta(home_stats, away_stats, outcome):
 
 def generate_examples():
 
-	games = open("textfiles/games.txt", "r")
-	output = open("training_data.txt", "w")
+	fileName = 'games_conf_2012-2013_formatted_unique'
+	outputName = 'training_data_2012-2013'
 
-	count = 0
+	games = open("textfiles/" + fileName + ".txt", "r")
+	output = open("textfiles/" + outputName + ".txt", "w")
+
+	# count = 0
 	for match in games:
-		count += 1
+		# count += 1
 
-		if count > 2000:
-			break
-		else:
-			split_line = match.strip("\n").split(",")
+		# if count > 2000:
+		# 	break
+		# else:
+		split_line = match.strip("\n").split(",")
 
-			home_team = split_line[0]
-			away_team = split_line[1]
-			outcome = split_line[2]
-			
-			home_stats, away_stats = get_stats(home_team, away_team)
+		home_team = split_line[0]
+		away_team = split_line[1]
+		outcome = split_line[2]
+		
+		home_stats, away_stats = get_stats(home_team, away_team)
 
-			# print home_team + " vs " + away_team
-			# print "home length: ", len(home_stats)
-			# print "away length: ", len(away_stats)
-			# print count
+		# print home_team + " vs " + away_team
+		# print "home length: ", len(home_stats)
+		# print "away length: ", len(away_stats)
+		# print count
 
-			# if (len(home_stats) == 0) or (len(away_stats) == 0):
-			# 	break
-			# Remove team name from the list
-			home_stats = home_stats[1:]
-			away_stats = away_stats[1:]
+		# if (len(home_stats) == 0) or (len(away_stats) == 0):
+		# 	break
+		# Remove team name from the list
+		home_stats = home_stats[1:]
+		away_stats = away_stats[1:]
 
-			
-			delta_stats = calc_delta(home_stats,away_stats,outcome)
+		
+		delta_stats = calc_delta(home_stats,away_stats,outcome)
 
-			delta_str = ",".join(map(str, delta_stats))
+		delta_str = ",".join(map(str, delta_stats))
 
-			output.write(delta_str + "\n")
+		output.write(delta_str + "\n")
 
 def main():
 	generate_examples()
